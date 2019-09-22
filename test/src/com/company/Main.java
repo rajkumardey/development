46904148ec891e5b09dfbd4412be4226710376b9
Raj <rajkumardey011@gmail.com>
2019-09-22 13:16:01 +0530

    change in main

--- test/src/com/company/Main.java
+++ test/src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.println("test for the git");
-	    System.out.println("test for the git");
+	    System.out.println("test fir the git");
     }
 }
